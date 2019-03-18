49bfce3dc5dcc832f6de05c7a92ed3ed4b78b207
Vyacheslav Alayev <valayev1@gmail.com>
2019-03-17 21:10:47 -0400

    Add head for seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,8 @@
 module.exports = {
   pathPrefix: "/appletreelabs-gatsby",
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-root-import']
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-root-import',
+    'gatsby-plugin-react-helmet'
+  ]
 }
--- src/components/Head.js
+++ src/components/Head.js
@@ -4,13 +4,12 @@ import favicon from "src/images/favicon.ico"
 
 export default function Head() {
   return (
-    <Helmet
-      title="Appletreelabs - The Product Laboratory"
-      meta={[
-        { name: "description", content: "The Product Laboratory" },
-        { name: "keywords", content: "digital, agency, product, laboratory, html, website" }
-      ]}
-      link={[{ rel: "shortcut icon", type: "image/png", href: favicon }]}
-    />
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Appletreelabs - The Product Laboratory</title>
+      <meta name="description" content="The Product Laboratory" />
+      <meta name="keywords" content="digital, agency, product, laboratory, html, website" />
+      <link rel="shortcut icon" type="image/png" href={favicon} />
+    </Helmet>
   )
 }
