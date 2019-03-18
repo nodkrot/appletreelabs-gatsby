eecec9ddb43936bf28e02c838d38a51588586d74
Vyacheslav Alayev <valayev1@gmail.com>
2019-03-17 22:27:26 -0400

    Fix

--- src/components/Head.js
+++ src/components/Head.js
@@ -4,12 +4,15 @@ import favicon from "../images/favicon.ico"
 
 export default function Head() {
   return (
-    <Helmet>
-      <meta charSet="utf-8" />
-      <title>Appletreelabs - The Product Laboratory</title>
-      <meta name="description" content="The Product Laboratory" />
-      <meta name="keywords" content="digital, agency, product, laboratory, html, website" />
-      <link rel="shortcut icon" type="image/png" href={favicon} />
-    </Helmet>
+    <Helmet
+      title="Appletreelabs - The Product Laboratory"
+      meta={[
+          { name: 'description', content: "The Product Laboratory" },
+          { name: 'keywords', content: "digital, agency, product, laboratory, html, website" },
+      ]}
+      link={[
+          { rel: 'shortcut icon', type: 'image/png', href: favicon }
+      ]}
+    />
   )
 }
